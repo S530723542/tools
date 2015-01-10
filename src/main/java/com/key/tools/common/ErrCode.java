e66d424eef7798364b0efbdd2a0fbdd1ffd5ac66
jishen.yjs <jishen.yjs@alibaba-inc.com>
2015-01-10 18:10:19 +0800

    finish hashpart

--- src/main/java/com/key/tools/common/ErrCode.java
+++ src/main/java/com/key/tools/common/ErrCode.java
@@ -4,24 +4,26 @@ public class ErrCode
 {
 	// 返回信息描述
 	public static final int SUCCESS = 0;
+	
+	public static final int SYSTEM_ERROR = -10001;
 
-	public static final int PHONE_EXIST = -1002;
+	public static final int PHONE_EXIST = -12002;
 
-	public static final int EMAIL_EXIST = -1003;
+	public static final int EMAIL_EXIST = -12003;
 
-	public static final int NAME_EXIST = -1004;
+	public static final int NAME_EXIST = -12004;
 
-	public static final int QQ_EXIST = -1005;
+	public static final int QQ_EXIST = -12005;
 
-	public static final int NOT_EXIST = -1101;
+	public static final int NOT_EXIST = -12101;
 
-	public static final int QQ_NOT_EXIT = -1105;
+	public static final int QQ_NOT_EXIT = -12105;
 
-	public static final int NOT_MATCH = -2001;
+	public static final int NOT_MATCH = -13001;
 
-	public static final int QQ_PASSWORD_WRONG = -2005;
+	public static final int QQ_PASSWORD_WRONG = -13005;
 
-	public static final int MORE_THAN_ONE = -3001;
+	public static final int MORE_THAN_ONE = -14001;
 
-	public static final int SYSTEM_ERROR = -4001;
+	
 }
